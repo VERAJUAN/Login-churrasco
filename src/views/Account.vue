330da91f3ac9dc542cb0810280eeba3f1582f093
Juan <verajuan78@gmail.com>
2020-01-05 21:15:45 -0300

    creacion componentes login y account

---
+++ src/views/Account.vue
@@ -0,0 +1,17 @@
+<template>
+
+<v-app-bar
+        app
+        color="blue"
+        dark
+>
+
+</v-app-bar>
+</template>
+
+
+<script>
+    export default {
+        name: 'Account'
+    }
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-navigation-drawer
+                absolute
+                dark
+                src="/img/login-bg.svg"
+                width="100%"
+                permanent
+        >
+
+        </v-navigation-drawer>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Login'
+    }
+</script>
